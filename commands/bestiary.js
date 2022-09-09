a6a022da3981021698363646efbb5957aab3ca1a
jakeahend <111516293+jakeahend@users.noreply.github.com>
2022-09-08 21:36:17 -0500

    remove pesky semicolons

--- commands/bestiary.js
+++ commands/bestiary.js
@@ -127,23 +127,23 @@ function generateClosestEorzeanWeatherChange(date) {
 // Generate the in-game weather number
 function getWeatherNumber(date) {
 	// Thanks to Rogueadyn's SaintCoinach library for this calculation.
-	const unixSeconds = parseInt(date / 1000);
+	const unixSeconds = parseInt(date / 1000)
 	// Get Eorzea hour for weather start
-	const bell = unixSeconds / 175;
+	const bell = unixSeconds / 175
 
 	// Do the magic 'cause for calculations 16:00 is 0, 00:00 is 8 and 08:00 is 16
-	const increment = (bell + 8 - (bell % 8)) % 24;
+	const increment = (bell + 8 - (bell % 8)) % 24
 
 	// Take Eorzea days since unix epoch
-	let totalDays = unixSeconds / 4200;
-	totalDays = (totalDays << 32) >>> 0;
+	let totalDays = unixSeconds / 4200
+	totalDays = (totalDays << 32) >>> 0
 
 	// 0x64 = 100
-	const calcBase = totalDays * 100 + increment;
+	const calcBase = totalDays * 100 + increment
 
 	// 0xB = 11
-	const step1 = ((calcBase << 11) ^ calcBase) >>> 0;
-	const step2 = ((step1 >>> 8) ^ step1) >>> 0;
+	const step1 = ((calcBase << 11) ^ calcBase) >>> 0
+	const step2 = ((step1 >>> 8) ^ step1) >>> 0
 
 	// 0x64 = 100
 	return step2 % 100
